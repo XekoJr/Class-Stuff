54d60724500f3cc9ca39282650dadb6481e69784
André Araújo Pacheco <101275618+XekoJr@users.noreply.github.com>
2024-05-10 18:36:42 +0100

    Reorganização

--- LojaJogos/src/GereLoja.java
+++
@@ -1,4 +0,0 @@
-
-public class GereLoja { //Todos os produtos são constituídos por um ou mais jogos.??? classe produto? que contem jogos. OUTRA DUVIDA - na  produtora só o diretor atual ou todos e o atual?
-
-}
